a1d1b2a6ee73b6c15c23c151ebdf412aa390b34e
William Okuno <okunowilliam@gmail.com>
2015-05-25 22:27:37 -0500

    start of something
    Why must i do all this right now

---
+++ app.js
@@ -0,0 +1,17 @@
+var PORT = 10552;
+var HOST = '23.228.235.96';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
