5921c775d0bd462f50a7076cd263e6a970eafdd2
William Okuno <beastyworld@gmail.com>
2015-05-26 02:46:56 -0500

    updated

--- test.js
+++ test.js
@@ -1,68 +1,75 @@
-var d3   = require('d3'),
-    http = require('http')
- 
-http.createServer(function (req, res) {
-  // Chrome automatically sends a requests for favicons
-  // Looks like https://code.google.com/p/chromium/issues/detail?id=39402 isn't
-  // fixed or this is a regression.
-  if(req.url.indexOf('favicon.ico') != -1) {
-    res.statusCode = 404
-    return
+var request = require('request');
+var querystring = require('querystring');
+
+
+// DEV
+
+var user = "erero7"; // Your plotly username or email. Sign up here: https://plot.ly/ssu
+var key = "uim7iw1p2r"; // Your plotly API key, view here: https://plot.ly/api/key
+var token1 = "i0qkzla0e2";
+var token2 = "ca12xrca78";
+var filename = "streaming second";
+var server = 'https://plot.ly/'; //'http://ec2-54-225-12-108.compute-1.amazonaws.com/'
+
+var data = [{
+    x:[1, 1, 1, 1, 1]
+  , y:[5, 6, 7, 8, 9]
+  , type:'scatter'
+  , mode:'lines'
+  , stream: {
+      token: token1
+    , maxpoints: 10
+    }
   }
- 
-  var pad     = { t: 10, r: 10, b: 50, l: 40 },
-      width   = 800 - pad.l - pad.r,
-      height  = 500 - pad.t - pad.b,
-      samples = d3.range(10).map(d3.random.normal(10, 5)),
-      x       = d3.scale.linear().domain([0, samples.length - 1]).range([0, width]),
-      y       = d3.scale.linear().domain([0, d3.max(samples)]).range([height, 0]),
-      xAxis   = d3.svg.axis().scale(x).orient('bottom').tickSize(height),
-      yAxis   = d3.svg.axis().scale(y).orient('left')
- 
-  var line = d3.svg.line()
-    .interpolate('basis')
-    .x(function(d, i) { return x(i) })
-    .y(y)
- 
-  var vis = d3.select('body').html('').append('svg')
-    .attr('xmlns', 'http://www.w3.org/2000/svg')
-    .attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
-    .attr('width', width + pad.l + pad.r)
-    .attr('height', height + pad.t + pad.b)
-  .append('g')
-    .attr('transform', 'translate(' + pad.l + ',' + pad.t + ')')
- 
-  vis.append('g')
-    .attr('class', 'x axis')
-    .call(xAxis)
- 
-  vis.append('g')
-    .attr('class', 'y axis')
-    .call(yAxis)
- 
-  vis.selectAll('.axis text')
-    .style('fill', '#888')
-    .style('font-family', 'Helvetica Neue')
-    .style('font-size', 11)
- 
-  vis.selectAll('.axis line')
-    .style('stroke', '#eee')
-    .style('stroke-width', 1)
- 
-  vis.selectAll('.domain')
-    .style('display', 'none')
- 
-  vis.selectAll('path.samples')
-    .data([samples])
-  .enter().append('path')
-    .attr('class', 'samples')
-    .attr('d', line)
-    .style('fill', 'none')
-    .style('stroke', '#c00')
-    .style('stroke-width', 2)
- 
-  res.writeHead(200, {'Content-Type': 'image/svg+xml'})
-  res.end(d3.select('body').node().innerHTML)
-}).listen(1337, '127.0.0.1')
- 
-console.log('Server running at http://127.0.0.1:1337/');
+  ,{
+    x:[2, 2, 2, 2, 2]
+  , y:[1, 2, 3, 4, 5]
+  , type: 'scatter'
+  , mode: 'lines'
+/*  , stream: {
+      token: token2
+    , maxpoints: 10
+    }*/
+  }
+]
+
+var query = {
+    un: user
+  , key: key
+  , origin: "plot"
+  , platform: "REST"
+  , args: JSON.stringify(data)
+  , kwargs: JSON.stringify({
+     filename: filename
+   , fileopt: "overwrite"
+    , layout: {
+        title: "streaming-demo"
+      , width : 600
+      , height: 600
+    }
+    , world_readable: true
+  })
+};
+
+
+request(
+    {
+        method: 'POST'
+      // , uri: 'https://plot.ly/clientresp'
+      , uri: server+'clientresp/'
+      , body:  querystring.stringify(query)
+    }
+  , function (err, res) {
+
+      if(res.statusCode == 200){
+        var payload = JSON.parse(res.body);
+        console.log(payload)
+      }
+
+      else {
+        console.log('error: '+ err, 'status', res.statusCode);
+      }
+
+    }
+
+)
